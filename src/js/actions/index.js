909f2d6427280a9b61de75aee4201689dc8517a7
angelorubin <angelorubin@gmail.com>
2019-05-01 19:20:38 -0300

    Initial commit

---
+++ src/js/actions/index.js
@@ -0,0 +1,8 @@
+import { ADD_ARTICLE } from "../actions/action-types";
+
+export function addArticle(payload) {
+  return {
+    type: ADD_ARTICLE,
+    payload
+  };
+}
---
+++ src/js/reducers/index.js
@@ -0,0 +1,25 @@
+import { ADD_ARTICLE } from "../constants/action-types";
+
+const initialState = {
+  articles: [
+    {
+      id: 1,
+      title: "my title 01"
+    },
+    {
+      id: 2,
+      title: "my title 02"
+    }
+  ]
+};
+
+const rootReducer = (state = initialState, action) => {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+  return state;
+};
+
+export default rootReducer;
