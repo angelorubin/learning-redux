6d384c3b752fe94e63694efda3a76f2fb2d179ca
angelorubin <angelorubin@gmail.com>
2019-05-01 21:51:49 -0300

    Import Form Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from "react";
 import List from "./js/components/List";
+import Form from "./js/components/Form";
 
 function App() {
   return (
     <>
       <h2>Learning Redux</h2>
+      <Form />
       <List />
     </>
   );
