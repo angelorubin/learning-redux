473878c5b6d6688e2b6d90d194c40781f6c9f526
angelorubin <angelorubin@gmail.com>
2019-05-02 16:41:02 -0300

    Removing constructor, switching native functions by arrow functions

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -3,51 +3,43 @@ import { connect } from "react-redux";
 import uuidv1 from "uuid";
 import { addArticle } from "../actions/index";
 
-function mapDispatchToProps(dispatch) {
+const mapDispatchToProps = dispatch => {
   return {
     addArticle: article => dispatch(addArticle(article))
   };
-}
+};
 
 class ConnectedForm extends Component {
-  constructor() {
-    super();
-    this.state = {
-      title: ""
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+  state = {
+    title: ""
+  };
 
-  handleChange(event) {
+  handleChange = event => {
     this.setState({ [event.target.id]: event.target.value });
-  }
+  };
 
-  handleSubmit(event) {
+  handleSubmit = event => {
     event.preventDefault();
     const { title } = this.state;
     const id = uuidv1();
     this.props.addArticle({ title, id });
     this.setState({ title: "" });
-  }
+  };
 
   render() {
     const { title } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
-        <div className="form-group">
+        <div>
           <label htmlFor="title">Title</label>
           <input
             type="text"
-            className="form-control"
             id="title"
             value={title}
             onChange={this.handleChange}
           />
         </div>
-        <button type="submit" className="btn btn-success btn-lg">
-          SAVE
-        </button>
+        <button type="submit">SAVE</button>
       </form>
     );
   }
