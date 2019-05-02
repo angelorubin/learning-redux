e57ac5e3ac7d0cf66705c744d1376aec452fe703
angelorubin <angelorubin@gmail.com>
2019-05-01 21:52:22 -0300

    Create Form Component

---
+++ src/js/components/Form.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { addArticle } from "../actions/index";
+
+export default class ConnectdForm extends Component {
+  render() {
+    return <div>Form</div>;
+  }
+}
