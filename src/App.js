f81c3f159b9e8021cf6d792bb56382099b6221b7
angelorubin <angelorubin@gmail.com>
2019-05-04 14:52:31 -0300

    Add CssBaseline, Typography and Header components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React, { Fragment } from "react";
 import List from "./js/components/List";
 import Form from "./js/components/Form";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Header from "./js/components/Header";
 
 function App() {
   return (
     <>
-      <h2>Learning Redux</h2>
-      <Form />
-      <List />
+      <CssBaseline>
+        <Header />
+        <Typography variant="h4" gutterBottom>
+          Learning Redux
+        </Typography>
+        <Form />
+        <List />
+      </CssBaseline>
     </>
   );
 }
