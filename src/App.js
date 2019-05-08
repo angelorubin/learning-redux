13a777766ec469613dcd6ad960ea4a0aa1a2dbed
angelorubin <angelorubin@gmail.com>
2019-05-08 18:12:33 -0300

    Fix material-ui problems

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { Fragment } from "react";
-import List from "./js/components/List";
-import Form from "./js/components/Form";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Typography from "@material-ui/core/Typography";
-import Header from "./js/components/Header";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <>
-      <CssBaseline>
-        <Header />
-        <Typography variant="h4" gutterBottom>
-          Learning Redux
-        </Typography>
-        <Form />
-        <List />
-      </CssBaseline>
-    </>
+    <Fragment>
+      <Header />
+    </Fragment>
   );
 }
 
