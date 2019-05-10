92516ad9f64b905bad35dcce2cfd4795cf5f6419
angelorubin <angelorubin@gmail.com>
2019-05-10 13:46:50 -0300

    Import Login component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Fragment } from "react";
-import Header from "./components/Header";
+import Login from "./components/Login";
 
 function App() {
   return (
     <Fragment>
-      <Header />
+      <Login />
     </Fragment>
   );
 }
