87a73132ef4a9f5f895e96ddc5466bd702af683b
angelorubin <angelorubin@gmail.com>
2019-05-12 22:07:10 -0300

    Using the redux compose method to activate the extension of redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,10 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import { rootReducer } from "../reducers";
 
-const loggerMiddleware = createLogger();
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(thunkMiddleware, loggerMiddleware)
+  composeEnhancer(applyMiddleware(thunkMiddleware))
 );
